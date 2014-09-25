b3b60c76ef1b3eba6d5aea32cd434ca9c292c767
Chris Newbery <cnewbery2@studentmail.nait.ca>
2014-09-25 12:05:02 -0600

    Added Bill and Waiter Entities

---
+++ eRestaurantSystem/Entities/Waiter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+    using System.ComponentModel.DataAnnotations;
+#endregion
+
+//GOOD TO KNOW
+//----------------------------------------------------
+//Time Span - Only holds time
+//DayOfWeek - Enum. (0 = Sunday through 6 = Saturday)
+
+namespace eRestaurantSystem.Entities
+{
+    public class Waiter
+    {
+        [Key]
+        public int WaiterID { get; set; }
+
+        //First Name
+        [Required(ErrorMessage="A first name is required.")]
+        [StringLength(25, ErrorMessage="First name can be up to 25 characters.")]
+        public string FirstName { get; set; }
+
+        //Last Name
+        [Required(ErrorMessage = "A last name is required.")]
+        [StringLength(35, ErrorMessage = "Last name can be up to 35 characters.")]
+        public string LastName { get; set; }
+
+        //Phone Number
+        [Required(ErrorMessage = "A phone number is required.")]
+        [StringLength(15, ErrorMessage = "Maximum 15 numbers.")]
+        public string Phone { get; set; }
+
+        //Address
+        [Required(ErrorMessage = "An address is required.")]
+        [StringLength(30, ErrorMessage = "Maximum 30 numbers.")]
+        public string Address { get; set; }
+
+        //Hire Date
+        [Required(ErrorMessage="A hire date must be entered.")]
+        public DateTime HireDate { get; set; }
+        
+        //Release Date
+        public DateTime ReleaseDate { get; set; }
+
+
+
+        //VIRTUAL NAVIGATIONS
+
+        //Bill
+        public virtual ICollection<Bill> BillsP { get; set; }
+        
+
+    }
+}
